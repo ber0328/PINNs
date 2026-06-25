a6fedf7888741c1562607f0bfca71cc44c424cfc
ber0328 <vaclav.bernatak.st@vsb.cz>
2026-06-25 20:45:47 +0200

    biot equation first steps

--- src/data/aquarium_domain.py
+++ src/data/aquarium_domain.py
@@ -8,6 +8,7 @@ class AquariumContext(cb.CubeContext):
     ball_centre: torch.Tensor = torch.tensor([])
     ball_r: float = 0.1
     ball_N: int = 100
+    ball_dim: int = -1
 
 
 class AquariumDomain(cb.CubeDomain):
@@ -25,13 +26,19 @@ class AquariumDomain(cb.CubeDomain):
         return torch.cat([square_pts, self.ball_boundary], dim=0)
 
     def _select_pts_outside(self):
-        mask = torch.sum((self.interior - self.ctx.ball_centre.squeeze())**2, dim=1) > self.ctx.ball_r**2
+        mask = torch.sum((self.interior[:, 0:self.ctx.ball_dim] - self.ctx.ball_centre.squeeze())**2, dim=1) > self.ctx.ball_r**2
         return self.interior[mask]
 
     def _gen_rand_ball_bnd(self):
-        ball_pts = self._gen_rand_bnd_tensor(torch.ones(self.ctx.dim, device=self.ctx.device),
-                                             torch.full((self.ctx.dim, ), -1, device=self.ctx.device),
-                                             (self.ctx.ball_N, self.ctx.dim))
+        ball_pts = self._gen_rand_bnd_tensor(torch.ones(self.ctx.ball_dim, device=self.ctx.device),
+                                             torch.full((self.ctx.ball_dim, ), -1, device=self.ctx.device),
+                                             (self.ctx.ball_N, self.ctx.ball_dim))
+        
         ball_pts_norm = torch.norm(ball_pts, p=2, dim=1).unsqueeze(1)
         ball_pts = (self.ctx.ball_r * ball_pts / ball_pts_norm) + self.ctx.ball_centre
-        return ball_pts
+        
+        rest_pts = self._gen_rand_bnd_tensor(torch.tensor(self.ctx.l_bounds[self.ctx.ball_dim:], device=self.ctx.device),
+                                             torch.tensor(self.ctx.u_bounds[self.ctx.ball_dim:], device=self.ctx.device),
+                                             (self.ctx.ball_N, self.ctx.dim - self.ctx.ball_dim))
+        
+        return torch.column_stack((ball_pts, rest_pts))
